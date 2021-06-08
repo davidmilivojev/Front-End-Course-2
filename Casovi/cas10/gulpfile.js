f4f25a51d172dcbc173363d9ba60ddad2eab3c3b
davidmilivojev <davidmilivojev@gmail.com>
2021-06-08 21:51:44 +0200

    cas 10

---
+++ Casovi/cas10/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var consolidate = require('gulp-consolidate');
+var iconfont = require('gulp-iconfont');
+
+// 1. scss to css task
+
+// 2. scss lint
+
+// 3. iconfont task
+
+// 4. copy html to dist
+
+
+//watcher
