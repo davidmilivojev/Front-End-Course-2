95dec40121286230f0c1e155e13a673f5af2d699
davidmilivojev <davidmilivojev@gmail.com>
2021-06-08 18:33:04 +0200

    cas 9 materija;

--- Casovi/cas9/js/main.js
+++ Casovi/cas9/js/main.js
@@ -1,1 +1,47 @@
-console.log('test');
+function equalHeight() {
+    $('.js-card').matchHeight();
+}
+
+function dropdown() {
+    $('select').selectric();
+}
+
+function slider() {
+    $('.js-slider').slick({
+        infinite: true,
+        slidesToShow: 3
+    });
+}
+
+function sliderSwiper() {
+    var swiper = new Swiper('.js-swiper', {
+        // Optional parameters
+        slidesPerView: 3,
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}
+
+$('.inp-btn').on('click', function() {
+    $(this).parent().addClass('hide');
+});
+
+sliderSwiper();
+slider();
+dropdown();
+equalHeight();
